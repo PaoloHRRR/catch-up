0c030316b0002702cd82092a4e4a98fccc599481
Paolo Henry Reaño Delgadillo <henry29.2004@hotmail.com>
2024-09-10 15:41:32 -0500

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rick and Morty API consumption
+This project consumes the Rick and Morty API using Vite + Vue 3
+In the project I use axios for program and API communication.
+In the future I will update the project with Prime Vue to display the information captured from the API
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import ramComponent from "./rick-and-morty/ram.component.vue";
+</script>
+
+<template>
+  <ram-component/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
